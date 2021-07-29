929273c40e780c5930bbe271cab255fdd9c15579
Haoyang Zhang <haoyangzhang@HaoyangdeMacBook-Pro.local>
2021-07-29 14:29:04 +0800

    init

---
+++ testbench.v
@@ -0,0 +1,45 @@
+
+`timescale 1ns / 1ns
+module testbench();
+  parameter half_clk_cycle = 1;
+  reg clk_p;
+  reg reset;
+  reg start;
+  reg [4:0] out;
+  wire ready;
+  wire correct;
+
+  main(reset, start, clk_p, out, ready);
+
+  initial
+  begin
+    clk_p = 0;
+    reset = 0;
+    #10;
+    reset = 1;
+    #10;
+    reset = 0;
+    start = 1;
+  end
+
+  always #2.5 clk_p = ~clk_p;
+
+
+  integer cycles;
+  initial cycles = 0;
+  always @(posedge clk_p) begin
+    cycles = cycles + 1;
+  end
+
+  integer f1;
+  integer f2;
+  always @(posedge ready) begin
+    f1 = $fopen("result.txt", "w");
+    f2 = $fopen("cycles.txt", "w");
+    #1  $fwrite(f1, "%1d\n", correct);
+        $fwrite(f2, "%d\n", cycles);
+    #1  $fclose(f1);
+        $fclose(f2);
+        $stop;
+  end
+endmodule
---
+++ trivial_fsm.v
@@ -0,0 +1,61 @@
+
+
+module main (
+    input reset,
+    input start,
+    input clk,
+    output reg [4:0] out,
+    output reg ready
+);
+    parameter state_one = 0;
+    parameter state_two = 1;
+    parameter state_three = 2;
+    parameter state_four = 3;
+    parameter state_five = 4;
+
+    reg[11:0] current_state;
+    reg[11:0] next_state;
+
+    always @(posedge clk) begin
+        if (reset)==1'b1 begin
+            current_state <= state_one;
+        end
+        else if (start==1'b1) begin
+            current_state <= next_state;
+        end
+        else begin
+            current_state <= current_state;
+        end
+    end
+
+    always @(*) begin
+        case (current_state) begin
+            state_one : begin
+                next_state = state_two;
+            end
+            state_two : begin
+                next_state = state_three;
+            end
+            state_three : begin
+                next_state = state_four;
+            end
+            state_four : begin
+                next_state = state_five;
+            end
+            state_five : begin
+                next_state = state_five;
+            end
+            default : begin
+                next_state = state_one;
+            end
+        end
+
+    end
+
+    assign out = current_state[4:0];
+    assign ready = (current_state==state_five);
+
+endmodule
+
+
+
